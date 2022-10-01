9791b5e6e49fcb663421d61ba8885acb7a0d4d8b
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2022-10-02 00:22:42 +0500

    added a fallback loading spinner

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Sidebar from '../../components/sidebar';
 import { getBlog } from '../api/hello';
 import Head from 'next/head';
-
+import { useRouter } from 'next/router';
 export async function getStaticPaths() {
   const response = await fetch('http://localhost:3000/api/get-blog-ids');
   const data = await response.json();
@@ -30,31 +30,47 @@ const Blog = ({ blog }) => {
     };
     getText();
   }, []);
-  return (
-    <>
-      <Sidebar />
-      <Head>
-        <title>{blog.title}</title>
-        <meta name='description' content={blog.description} />
-        <link rel='icon' href='/logo.png' />
-        <meta name='robots' content='index, follow, archive' />
-        <meta property='og:type' content='artical' />
-        <meta property='og:title' content={blog.title} />
-        <meta property='og:site_name' content='Bitsframe' />
-        <meta name='viewport' content='width=device-width,initial-scale=1.0' />
-      </Head>
-      <main className='flex justify-center relative top-10 min-h-[70vh]'>
-        <section className='lg:w-4/5 flex items-center flex-col w-[95vw] space-y-10'>
-          <h1 className='text-5xl font-bold'>{blog.title}</h1>
+  const router = useRouter();
+  if (router.isFallback)
+    return (
+      <div class='w-[100vw] h-[100vh] flex justify-center items-center'>
+        <div
+          class='spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full'
+          role='status'
+        >
+          <span class='visually-hidden'>Loading...</span>
+        </div>
+      </div>
+    );
+  else
+    return (
+      <>
+        <Sidebar />
+        <Head>
+          <title>{blog.title}</title>
+          <meta name='description' content={blog.description} />
+          <link rel='icon' href='/logo.png' />
+          <meta name='robots' content='index, follow, archive' />
+          <meta property='og:type' content='artical' />
+          <meta property='og:title' content={blog.title} />
+          <meta property='og:site_name' content='Bitsframe' />
+          <meta
+            name='viewport'
+            content='width=device-width,initial-scale=1.0'
+          />
+        </Head>
+        <main className='flex justify-center relative top-10 min-h-[70vh]'>
+          <section className='lg:w-4/5 flex items-center flex-col w-[95vw] space-y-10'>
+            <h1 className='text-5xl font-bold'>{blog.title}</h1>
 
-          <article
-            className='content text-gray-700'
-            dangerouslySetInnerHTML={{ __html: text }}
-          ></article>
-        </section>
-      </main>
-    </>
-  );
+            <article
+              className='content text-gray-700'
+              dangerouslySetInnerHTML={{ __html: text }}
+            ></article>
+          </section>
+        </main>
+      </>
+    );
 };
 
 export default Blog;
