933de8efef9c4a7c2351e7d975daa25ca8889d3c
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2022-10-19 20:01:43 +0500

    deployment

--- server.js
+++ server.js
@@ -1,37 +1,69 @@
 // server.js
-const { createServer } = require('http');
-const { parse } = require('url');
+const functions = require('firebase-functions');
+// const { createServer } = require('http');
+// const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = dev ? 'localhost' : 'bitsframe.com';
-const port = process.env.PORT || 2083;
+const hostname = dev ? 'localhost' : 'bitsrame.web.app';
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+const app = next({ dev, hostname, port, conf: { distDir: '.next' } });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
+exports.nextServer = functions
+  .region('asia-southeast1')
+  .https.onRequest((req, res) => {
+    return app.prepare().then(() => {
+      createServer(async (req, res) => {
+        try {
+          // Be sure to pass `true` as the second argument to `url.parse`.
+          // This tells it to parse the query portion of the URL.
+          const parsedUrl = parse(req.url, true);
+          const { pathname, query } = parsedUrl;
 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query);
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query);
-      } else {
-        await handle(req, res, parsedUrl);
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err);
-      res.statusCode = 500;
-      res.end('internal server error');
-    }
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+          if (pathname === '/a') {
+            await app.render(req, res, '/a', query);
+          } else if (pathname === '/b') {
+            await app.render(req, res, '/b', query);
+          } else {
+            await handle(req, res, parsedUrl);
+          }
+        } catch (err) {
+          console.error('Error occurred handling', req.url, err);
+          res.statusCode = 500;
+          res.end('internal server error');
+        }
+      }).listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://${hostname}:${port}`);
+      });
+      // return handle(req, res);
+    });
   });
-});
+
+// app.prepare().then(() => {
+//   createServer(async (req, res) => {
+//     try {
+//       // Be sure to pass `true` as the second argument to `url.parse`.
+//       // This tells it to parse the query portion of the URL.
+//       const parsedUrl = parse(req.url, true);
+//       const { pathname, query } = parsedUrl;
+
+//       if (pathname === '/a') {
+//         await app.render(req, res, '/a', query);
+//       } else if (pathname === '/b') {
+//         await app.render(req, res, '/b', query);
+//       } else {
+//         await handle(req, res, parsedUrl);
+//       }
+//     } catch (err) {
+//       console.error('Error occurred handling', req.url, err);
+//       res.statusCode = 500;
+//       res.end('internal server error');
+//     }
+//   }).listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://${hostname}:${port}`);
+//   });
+// });
