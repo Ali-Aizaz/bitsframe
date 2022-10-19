db7c00b5c044c3b0b4aa78dad23acf842873d490
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2022-10-19 21:22:15 +0500

    successfull deploy

--- server.js
+++ server.js
@@ -5,40 +5,40 @@ const functions = require('firebase-functions');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = dev ? 'localhost' : 'bitsrame.web.app';
-const port = process.env.PORT || 3000;
+// const hostname = dev ? 'localhost' : 'bitsrame.web.app';
+// const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port, conf: { distDir: '.next' } });
+const app = next({ dev, conf: { distDir: '.next' } });
 const handle = app.getRequestHandler();
 
 exports.nextServer = functions
   .region('asia-southeast1')
   .https.onRequest((req, res) => {
     return app.prepare().then(() => {
-      createServer(async (req, res) => {
-        try {
-          // Be sure to pass `true` as the second argument to `url.parse`.
-          // This tells it to parse the query portion of the URL.
-          const parsedUrl = parse(req.url, true);
-          const { pathname, query } = parsedUrl;
+      // createServer(async (req, res) => {
+      //   try {
+      //     // Be sure to pass `true` as the second argument to `url.parse`.
+      //     // This tells it to parse the query portion of the URL.
+      //     const parsedUrl = parse(req.url, true);
+      //     const { pathname, query } = parsedUrl;
 
-          if (pathname === '/a') {
-            await app.render(req, res, '/a', query);
-          } else if (pathname === '/b') {
-            await app.render(req, res, '/b', query);
-          } else {
-            await handle(req, res, parsedUrl);
-          }
-        } catch (err) {
-          console.error('Error occurred handling', req.url, err);
-          res.statusCode = 500;
-          res.end('internal server error');
-        }
-      }).listen(port, (err) => {
-        if (err) throw err;
-        console.log(`> Ready on http://${hostname}:${port}`);
-      });
-      // return handle(req, res);
+      //     if (pathname === '/a') {
+      //       await app.render(req, res, '/a', query);
+      //     } else if (pathname === '/b') {
+      //       await app.render(req, res, '/b', query);
+      //     } else {
+      //       await handle(req, res, parsedUrl);
+      //     }
+      //   } catch (err) {
+      //     console.error('Error occurred handling', req.url, err);
+      //     res.statusCode = 500;
+      //     res.end('internal server error');
+      //   }
+      // }).listen(port, (err) => {
+      //   if (err) throw err;
+      //   console.log(`> Ready on http://${hostname}:${port}`);
+      // });
+      return handle(req, res);
     });
   });
 
